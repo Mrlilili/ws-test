14f220e8b8b7bd371b3e569936bc967250e45ccf
liyan1 <liyan1@joyy.com>
2024-08-16 23:27:08 +0800

    chore: test add-unplugin-auto-import

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
+const computedCount = computed(() => {
+  return;
+});
 </script>
 
 <template>
