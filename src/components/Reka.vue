89ab1f29ca3946f58a7c54b6d7aacb44cfa5a2de
liyan1 <liyan1@joyy.com>
2025-08-14 14:35:59 +0800

    chore: reka2.4.1 demo

---
+++ src/components/Reka.vue
@@ -0,0 +1,11 @@
+<template>
+  <DialogRoot>
+    <DialogOverlay />
+    <DialogContent></DialogContent>
+  </DialogRoot>
+</template>
+<script setup lang="ts">
+import { DialogRoot, DialogOverlay, DialogContent } from 'reka-ui'
+
+defineOptions({ name: 'Reka' })
+</script>
